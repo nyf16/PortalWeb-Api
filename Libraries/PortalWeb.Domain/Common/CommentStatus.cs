d20c88b69d7191be7a8f3176a70d9ca9a505b655
Onur FERIK <49190459+nyf16@users.noreply.github.com>
2020-11-12 21:36:40 +0300

    Entity Modelleri eklendi

---
+++ Libraries/PortalWeb.Domain/Common/CommentStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PortalWeb.Domain.Common
+{
+    public enum CommentStatus
+    {
+        Pending = 2,
+        Approved = 4,
+        NotApproved = 6
+    }
+}
---
+++ Libraries/PortalWeb.Domain/Entities/Contact.cs
@@ -0,0 +1,23 @@
+﻿using PortalWeb.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PortalWeb.Domain.Entities
+{
+    public class Contact : BaseEntity
+    {
+        public int? UserId { get; set; }
+
+        public virtual User User { get; set; }
+
+        public string FullName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string Content { get; set; }
+
+    }
+}
